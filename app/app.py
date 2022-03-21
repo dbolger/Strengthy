2cd5ffcc0ee2223840446f4f8feb9cd69d81e304
FivePixels <o5pxels@gmail.com>
2022-03-21 18:06:38 -0500

    Add user endpoints, add placeholders for login/register, update
    hyperlinks to refer to correct endpoints

--- app/app.py
+++ app/app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def index():
     return render_template('base/index.html')
+
+@app.route("/login", methods=["GET"])
+def login():
+    return render_template('user/login.html')
+
+@app.route("/register", methods=["GET"])
+def register():
+    return render_template('user/register.html')
