2dc93f6bcfa14dec4eac7c6b1a3f0d83e43b48ac
stilbruch <stilbruch@protonmail.com>
2022-05-09 11:46:00 -0500

    Fix "sets completed" on homescreen

--- app/routes/basic.py
+++ app/routes/basic.py
@@ -1,7 +1,7 @@
 from app import app, db
 from flask import render_template
 from flask_login import current_user, login_required
-from tables import Workout, WorkoutRecord
+from tables import Workout, WorkoutRecord, SetRecord
 
 
 @app.route("/", methods=["GET"])
@@ -19,4 +19,13 @@ def home():
         .order_by(WorkoutRecord.finished.desc())
     )
 
-    return render_template("home.html", records=records)
+    sets_completed = (
+        db.session.query(SetRecord, WorkoutRecord)
+        .filter(
+            WorkoutRecord.id == SetRecord.workout_record_id,
+            WorkoutRecord.user_id == current_user.id,
+        )
+        .count()
+    )
+
+    return render_template("home.html", records=records, sets_completed=sets_completed)
